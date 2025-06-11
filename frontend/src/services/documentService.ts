fd0dac49cc4f03b0f5e5a0a7241b37d7dbdf800a
StevenJakson <imac2019@StevenJaksons-iMac.local>
2025-06-11 19:20:41 +0330

    auto: 2025-06-11 19:20

--- frontend/src/services/documentService.ts
+++ frontend/src/services/documentService.ts
@@ -2,7 +2,7 @@
 // سرویس مدیریت مستندات در ایجنت هادربون
 
 import axios from 'axios';
-import { API_URL } from '..config';
+import { API_URL } from '../configconfig';
 import { getAuthHeader } from '../utils/auth';
 
 /**
