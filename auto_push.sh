a6418d0af2153131978c6d4845408bf447b0c5ed
StevenJakson <imac2019@StevenJaksons-iMac.local>
2025-06-11 18:00:11 +0330

    auto: 2025-06-11 18:00

--- auto_push.sh
+++ auto_push.sh
@@ -1,5 +1,3 @@
-PROJECT="/Users/imac2019/My-Apps/Haderboon" && \
-cat > "$PROJECT/auto_push.sh" <<'EOS'
 #!/bin/zsh
 cd "$(dirname "$0")" || exit 1
 [ -f .git/index.lock ] && rm -f .git/index.lock
@@ -8,28 +6,3 @@ if ! git diff --quiet ; then
   git commit -m "auto: $(date '+%Y-%m-%d %H:%M')"
   git push origin main
 fi
-EOS
-chmod +x "$PROJECT/auto_push.sh" && \
-PLIST="$HOME/Library/LaunchAgents/com.c123ir.autopush.haderboon.plist" && \
-cat > "$PLIST" <<EOS
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-  <key>Label</key><string>com.c123ir.autopush.haderboon</string>
-  <key>ProgramArguments</key>
-  <array>
-    <string>/bin/zsh</string>
-    <string>$PROJECT/auto_push.sh</string>
-  </array>
-  <key>WatchPaths</key>
-  <array>
-    <string>$PROJECT</string>
-  </array>
-  <key>RunAtLoad</key><true/>
-  <key>StandardOutPath</key><string>/tmp/autopush_haderboon.out</string>
-  <key>StandardErrorPath</key><string>/tmp/autopush_haderboon.err</string>
-</dict>
-</plist>
-EOS
-launchctl bootstrap gui/$(id -u) "$PLIST"
