e3d64a611f53e2efc20ad13ff81f877e0c8b4ee4
Mojtaba Hassani <123.hasani@gmail.com>
2025-06-13 18:12:01 +0330

    auto: 2025-06-13 18:12

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 // frontend/src/pages/HomePage.tsx
 
 import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import {
   FolderIcon,
   ChatBubbleLeftRightIcon,
