71a190ac1e091f1820121426813d016ecca10307
Mojtaba Hassani <123.hasani@gmail.com>
2025-06-13 11:14:08 +0330

    auto: 2025-06-13 11:14

--- backend/dist/interfaces/IAIProvider.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// backend/src/interfaces/IAIProvider.ts
-// واسط مشترک برای سرویس‌دهنده‌های هوش مصنوعی
-Object.defineProperty(exports, "__esModule", { value: true });
--- test.md
+++ test.md
@@ -1,4 +1,8 @@
 hello Wed Jun 11 17:55:54 +0330 2025
+
+DATABASE_URL="postgresql://haderboon_user:2123@localhost:5432/haderboon_db?schema=public"
+
+
   
   dfsgsfd
   زی
