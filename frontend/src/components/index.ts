fb45cc9dc5c5b9b93d3a533c8e87677100ec3175
Mojtaba Hassani <123.hasani@gmail.com>
2025-06-16 15:53:03 +0330

    auto: 2025-06-16 15:53

--- frontend/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-// components/index.ts - فایل صحیح برای export کامپوننت‌ها
-export { default as FileTree } from './FileTree';
-export { default as FileContentViewer } from './FileContentViewer';
-export { default as WatchingStatus } from './WatchingStatus';
-export { default as ProjectFileManager } from './ProjectFileManager';
