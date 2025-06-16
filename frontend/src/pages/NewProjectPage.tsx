c293c237ddabe6b075a07e208eeb952b280c371d
Mojtaba Hassani <123.hasani@gmail.com>
2025-06-16 13:01:23 +0330

    auto: 2025-06-16 13:01

--- frontend/src/pages/NewProjectPage.tsx
+++ frontend/src/pages/NewProjectPage.tsx
@@ -667,8 +667,7 @@ const NewProjectPage: React.FC = () => {
                   type="file"
                   ref={directoryInputRef}
                   onChange={handleDirectoryInput}
-                  webkitdirectory=""
-                  directory=""
+                  {...({ webkitdirectory: '' } as any)}
                   multiple
                   className="hidden"
                   disabled={isUploading}
